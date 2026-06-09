56992fad57b98940ccb7f3a78abaa9bdb2f842c7
Ivana Ignjatic <ignjaticivana70@gmail.com>
2026-06-09 13:48:32 +0200

    [ADD] structure for records saving

---
+++ Server/Features/Commands/IngestDataCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Server.DTO;
+
+namespace Server.Features.Commands
+{
+    public record IngestDataCommand(IngestDataDTO Data) : IRequest<bool>;
+}
---
+++ Server/Features/Handlers/IngestDataHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Server.Features.Commands;
+
+namespace Server.Features.Handlers
+{
+    public class IngestDataHandler : IRequestHandler<IngestDataCommand, bool>
+    {
+        public Task<bool> Handle(IngestDataCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
